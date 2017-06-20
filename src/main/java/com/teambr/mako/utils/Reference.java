650fa0b8b939085477ef08276b468d110d5c0506
Buuz135 <buuz135@hotmail.com>
2017-06-20 15:25:58 +0200

    Mod Basics

---
+++ src/main/java/com/teambr/mako/utils/Reference.java
@@ -0,0 +1,9 @@
+package com.teambr.mako.utils;
+
+public class Reference {
+
+    public static final String MODID = "materialized";
+    public static final String VERSION = "0.1";
+    public static final String PROXY_CLIENT = "com.buuz135.materialized.proxy.client.ClientProxy";
+    public static final String PROXY_COMMON = "com.buuz135.materialized.proxy.CommonProxy";
+}
